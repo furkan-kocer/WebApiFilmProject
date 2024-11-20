8a45fe3ee8f834ff81c76987f7e68277e157ae18
furkan-kocer <83519498+furkan-kocer@users.noreply.github.com>
2024-11-20 23:08:03 +0300

    CustomProblemDetails Fac. Added
    To be able to return more reasonable errors to the end user custom problemdetails factory added into the project.

---
+++ FilmProject.Api/ApiReturnControls/CustomProblemDetailsFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmProject.Api.ApiReturnControls
+{
+    public class CustomProblemDetailsFactory : ProblemDetails
+    {
+        public static ProblemDetails CreateProblemDetails(
+        HttpContext httpContext,
+        int? statusCode = null,
+        string title = null,
+        string type = null,
+        string detail = null,
+        string instance = null)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode ?? 500,
+                Title = title,
+                Type = type,
+                Detail = detail,
+                Instance = instance
+            };
+            return problemDetails;
+        }
+    }
+}
