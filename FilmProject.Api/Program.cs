2e6a9aa86d91a99e3442dea67b6131c8b49dcc50
furkan-kocer <83519498+furkan-kocer@users.noreply.github.com>
2024-11-20 23:00:31 +0300

    Logging Configuration Applied
    SeriLog logging configuration settings applied into program.cs and necessary packages installed

--- FilmProject.Api/Program.cs
+++ FilmProject.Api/Program.cs
@@ -1,10 +1,15 @@
 using FilmProject.DataAccess;
 using FilmProject.DataAccess.CollectionRepositories.FilmCollection;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure the mongoDBSettings
 builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
+//Logging Configuration
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddJsonFile("appsettings.Development.json", optional: true).Build();
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+builder.Host.UseSerilog();
 // Add services to the container.
 builder.Services.AddSingleton<MongoDBService>();
 builder.Services.AddScoped<IFilmCollectionRepository,FilmCollectionRepository>();
@@ -22,10 +27,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Ensure proper log flushing on application shutdown
+var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+lifetime.ApplicationStopped.Register(Log.CloseAndFlush);
 
 app.Run();
